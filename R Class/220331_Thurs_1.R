66b99de423e9d0a66feb1385d91be11a6c645d65
unknown <jason2133@likelion.org>
2022-04-05 12:27:55 +0900

    R programming

---
+++ R Class/220331_Thurs_1.R
@@ -0,0 +1,40 @@
+1
+a
+'a'
+'1'
+
+class(1)
+class('1')
+
+as.numeric('1') + as.numeric('2')
+
+1; 2; 'a'
+
+# Arthmetic Operations
+2 + 1
+2 - 1
+2 * 1
+2 / 1
+2^3
+
+# Logical Expression
+3 < 2
+3 == 1 + 2
+3 != 1 + 2
+'A' == 'a'
+
+# Scalar Variable
+a <- 3
+a
+b = 3
+b
+
+var1 <- 'aaa'
+var2 <- 111
+var1
+var2
+
+getwd()
+
+
+
